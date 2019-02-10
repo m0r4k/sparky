95f27d27c8398ed1b789e397050e38db9deffa20
m0r4k <mor4k@outlook.com>
2019-02-10 21:40:58 +0100

    move

--- ufw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo ufw default allow outgoing
-sudo ufw default deny incoming
-sudo ufw allow ssh/tcp
-sudo ufw limit ssh/tcp
-sudo ufw allow 8890/tcp
-sudo ufw logging on
-sudo ufw enable
